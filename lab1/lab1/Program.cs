fcbe2299c84a52aa92695115b299fbecd855c86e
AlinaSeitbekova <alina.s261297@gmail.com>
2016-02-06 15:04:22 +0600

    lab1!!

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -8,35 +8,47 @@ namespace ConsoleApplication2
     class Program
     {
 
-        static void Main(string[] args)
+        static bool Isnotprime(string s)// создаем функцию
         {
-            for (int i = 0; i < args.Length; ++i)//строка состоит из чисел,будет проходить по каждому элементу(число),пока не дойдет до конца строки
-            {
-                int cnt = 0;
+            bool div = false;
+            //for (int i = 0; i < args.Length; ++i)//строка состоит из чисел,будет проходить по каждому элементу(число),пока не дойдет до конца строки
+            //{
+                
 
-                int x = int.Parse(args[i]);// преобразует строковое представление числа в числовое 
+                int x = int.Parse(s);// преобразует строковое представление числа в числовое 
                 for (int j = 2; j <= Math.Sqrt(x); ++j)//чтобы оценить на простоту-проверяем делители числа от 2 до корня из числа,потому что дальше нет смысла проверять
                 {
                     if (x % j == 0)//если есть делители то не простое число 
                     {
-                        cnt++;
+                        div = true;
+                        break;// выходит из цикла ,так как уже есть делители
+
                     }
                 }
-
-
-
-                if (cnt != 0)// если есть делители то не простое
-                {
-                    Console.WriteLine(x + "is not prime");
-                }
-
-                else// если нет делителей то простое
-                {
-                    Console.WriteLine(x + "is prime");
-                }
-                //  Console.ReadKey();
-
+                return (div);// true or false
             }
+        
+
+
+       static void Main(string[] args2)// создаем функцию
+       {        string str=Console.ReadLine();
+                string[] args =str.Split(' ');
+
+                for(int i=0;i<args.Length;i++)
+                {
+                    if( Isnotprime (args[i]))
+
+                        Console.WriteLine(args[i] + "is not prime");
+  
+
+                    else // если есть делители то не простое
+                {
+                    Console.WriteLine(args[i] + "is prime");
+                }
+
+           
+
+                }
         }
     }
 }
