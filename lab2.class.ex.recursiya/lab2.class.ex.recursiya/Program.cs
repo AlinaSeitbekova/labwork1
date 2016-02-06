7f4261fc9f4afc76a3ad15a07e60e05445a886e5
AlinaSeitbekova <alina.s261297@gmail.com>
2016-02-06 10:27:52 +0600

    lab2.inclass.ex.recursiya.getfiles

---
+++ lab2.class.ex.recursiya/lab2.class.ex.recursiya/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace SearchText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            DirectoryInfo dir = new DirectoryInfo(path);
+            Search(dir);
+        }
+        static void Search(DirectoryInfo dir)
+        {
+            int cnt = dir.GetFiles().Length;
+            Console.WriteLine(cnt + " files are located in " + dir.FullName);
+            foreach (DirectoryInfo ndir in dir.GetDirectories())
+            {
+                Search(ndir);
+            }
+        }
+    }
+}
